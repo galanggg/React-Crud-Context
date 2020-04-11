eba06bb712435d8c3e2b6ea9faae919783c62a0d
galanggg <tugasgalang@gmail.com>
2020-04-11 21:43:49 +0800

    Add new component (add,edit)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import {Route, Switch} from 'react-router-dom'
 import '../src/css/tailwind.css'
 import {GlobalProvider} from './context/GlobalState'
 import Home from './components/Home'
+import AddEmployee from './components/AddEmployee'
+import EditEmployee from './components/EditEmployee'
+
 
 function App() {
   return(
     <GlobalProvider>
       <Switch>
         <Route path="/" exact component={Home} />
-      </Switch>
+        <Route path="/add" exact component={AddEmployee} />
+        <Route path="/edit/:id" exact component={EditEmployee} />
+        </Switch>
     </GlobalProvider>
   )
 }
